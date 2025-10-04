46c02b981b2bf78ca5c206540dac8af7fbfd536b
Nagord <46509577+Nagord@users.noreply.github.com>
2025-10-03 22:26:32 -0700

    Updated documentation

--- FasterSwap/MyPluginInfo.cs
+++ FasterSwap/MyPluginInfo.cs
@@ -7,11 +7,11 @@ namespace FasterSwap
         public const string PLUGIN_GUID = "id107.fasterswap";
         public const string PLUGIN_NAME = "FasterSwap";
         public const string USERS_PLUGIN_NAME = "Faster Swap";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.0.1";
         public const string PLUGIN_DESCRIPTION = "Swaps items faster than vanilla";
         public const string PLUGIN_ORIGINAL_AUTHOR = "18107";
         public const string PLUGIN_AUTHORS = "18107";
-        public const string PLUGIN_THUNDERSTORE_ID = "";
+        public const string PLUGIN_THUNDERSTORE_ID = "NihilityShift/Faster_Swap";
     }
 }
 #pragma warning restore CS1591
